dac23494697b6f916367f1abec41013e94e575c0
zhaoyiping <zhaoyiping0622@gmail.com>
2023-12-05 18:21:06 +0800

    上个多线程，调调参

--- src/rootserver/ob_create_schema_parallel.cpp
+++ src/rootserver/ob_create_schema_parallel.cpp
@@ -13,7 +13,7 @@
 #define USING_LOG_PREFIX BOOTSTRAP
 
 namespace oceanbase {
-const int CreateSchemaParallelCnt = 1;
+const int CreateSchemaParallelCnt = 8;
 OBCreateSchemaParallel::OBCreateSchemaParallel(std::function<void()> func)
     : func_(func) {}
 OBCreateSchemaParallel::~OBCreateSchemaParallel() {}
